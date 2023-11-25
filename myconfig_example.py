b35f06b034c7715bc46f58b04bd8e59e88683026
LeBomb <cambon.olivier@gmail.com>
2023-11-25 20:41:16 +0100

    Add myconig example

---
+++ myconfig_example.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+
+""" MyConfig """
+
+
+SMS = ["+00000000000", "+00000000000"]
+EMAIL = ["myemail@gmail.com", "myemail@gmail.com"]
+
+EMAIL_SMTP = "smtp.free.fr"
+EMAIL_LOGIN = "myemail@free.fr"
+EMAIL_PASSWORD = "mypassword"
+EMAIL_SENDER = "Jeedom Home <" + EMAIL_LOGIN + ">"
