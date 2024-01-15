c5515abfde7725407e9c12e3758f894dc8ffe76a
LeBomb <cambon.olivier@gmail.com>
2024-01-16 00:15:31 +0100

    Correct lbemail to send to multiple recipient

--- lbemail.py
+++ lbemail.py
@@ -6,6 +6,7 @@
 
 
 import smtplib
+from email.mime.text import MIMEText
 import urllib.parse
 
 import settings
@@ -13,22 +14,19 @@ import fct
 import myconfig
 
 
-def sendto(email, object, msg):
+def sendto(email, subject, body):
     try:
-        object = urllib.parse.unquote_plus(object)
-        content = urllib.parse.unquote_plus(msg)
+        subject = urllib.parse.unquote_plus(subject)
+        body = urllib.parse.unquote_plus(body)
+        msg = MIMEText(body)
+        msg["Subject"] = str(subject)
+        msg["From"] = 'Jeedom ' + settings.HOSTNAME + ' <' + myconfig.EMAIL_LOGIN + '>'
+        msg["To"] = ", ".join(email)
         mail = smtplib.SMTP(myconfig.EMAIL_SMTP, 587, timeout=5.0)
         mail.ehlo()
         mail.starttls()
-        sender = myconfig.EMAIL_LOGIN
-        recipient = str(email)
-        recipient = recipient.replace("[","").replace("]","").replace("'","")
         mail.login(myconfig.EMAIL_LOGIN, myconfig.EMAIL_PASSWORD)
-        header = 'To:' + str(recipient) + '\n' \
-        + 'From:Jeedom ' + settings.HOSTNAME + ' <' + myconfig.EMAIL_LOGIN + '>\n' \
-        + 'subject:' + object + '\n'
-        content = header + content
-        mail.sendmail(sender, recipient, content)
+        mail.sendmail(myconfig.EMAIL_LOGIN, email, msg.as_string())
         mail.close()
     except Exception as ex:
         fct.log_exception(ex)
